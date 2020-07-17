464edfe1a510c0d7892de730d8ccc4759f3a60fe
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-17 22:50:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -108,7 +108,12 @@ class TicTacToe
     index = won?(board)
     if index == false 
       return nil
-      
+    else 
+      if board[index[0]] == "X"
+        return "X"
+      else 
+        return "O"
+      end 
     end 
   end 
   
