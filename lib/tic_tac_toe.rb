5d8ee5f8cbe3f5790c49dbfbab2023ce21aa67bf
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-17 22:44:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -87,8 +87,12 @@ class TicTacToe
     board.all? {|index| index == "X" || index == "O"}
   end 
   
-  def draw?
-    if !wo
+  def draw?(board)
+    if !won?(board) && full?(board)
+      return true 
+    else 
+      return false 
+    end 
   end 
   
   def over?
