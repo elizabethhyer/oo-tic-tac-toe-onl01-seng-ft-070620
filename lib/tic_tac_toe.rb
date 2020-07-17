1af731423b9dbfe1aa33b74120f0eba2b73ff60a
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-17 22:12:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,7 +76,7 @@ class TicTacToe
   
   def won? 
       WIN_COMBINATIONS.each do |winning_board|
-    if position_taken?(winning_board[0]) && @board[winning_board[0]] == 
+    if position_taken?(winning_board[0]) && @board[winning_board[0]] == @board[winning_board[1]] && @board[winning_board[1]] == @board[winning_board[2]]
     end 
     end 
   end 
