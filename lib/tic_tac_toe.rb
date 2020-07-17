ac372ee5441fcd07d5a96cec1420d60f9c12b480
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-17 20:27:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,12 +62,6 @@ class TicTacToe
   def turn 
    puts "Please enter a number between 1 and 9" 
    user_input = input_to_index(gets.chomp) 
-   if user_input.valid_move?(user_input)
-     current_player
-   else 
-     turn 
-   end 
-   display_board
   end 
   
 end 
